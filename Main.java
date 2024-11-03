e6c3ef6c85a0edc7cc84073a94f0c09f2519a7e5
José Francisco <84079883+jose-fps@users.noreply.github.com>
2024-11-03 20:47:41 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -29,3 +29,30 @@ public class Main {
 	}
 
 }
+
+
+Exercicio correto.
+
+ import java.util.Locale;
+ public class Main{
+ public staticvoid main(String[] args) {
+ String product1 = "Computer";
+ String product2 = "Office desk";
+ byteage= 30;
+ intcode= 5290;
+ chargender= 'F';
+ double price1= 2100.0;
+ double price2= 650.50;
+ double measure= 53.234567;
+ System.out.println("Products:");
+ System.out.printf("%s, which price is $ %.2f%n", product1, price1);
+ System.out.printf("%s, which price is $ %.2f%n", product2, price2);
+ System.out.println();
+ System.out.printf("Record: %d years old, code %d and gender: %c%n", age, code, gender);
+ System.out.println();
+ System.out.printf("Measue with eight decimal places: %.8f%n", measure);
+ System.out.printf("Rouded (three decimal places): %.3f%n", measure);
+ Locale.setDefault(Locale.US);
+ System.out.printf("US decimal point: %.3f%n", measure);
+ }
+ }
